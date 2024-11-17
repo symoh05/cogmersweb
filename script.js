fabe705746abde46ed4f98ddf68b7f987e08891d
symoh05 <sngugi175@gmail.com>
2024-11-17 13:09:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -18,26 +18,6 @@
      }
  });
 
-
- // Select the hamburger menu and off-screen menu
- const hamMenu = document.querySelector('.ham-menu');
- const offScreenMenu = document.querySelector('.off-screen-menu');
-
- // Toggle the menu when clicking on the hamburger menu
- hamMenu.addEventListener('click', (event) => {
-     event.stopPropagation(); // Prevent the event from bubbling up
-     hamMenu.classList.toggle('active');
-     offScreenMenu.classList.toggle('active');
- });
-
- // Close the menu if you click outside the menu
- document.addEventListener('click', (event) => {
-     if (!hamMenu.contains(event.target) && !offScreenMenu.contains(event.target)) {
-         hamMenu.classList.remove('active');
-         offScreenMenu.classList.remove('active');
-     }
- });
-
  document.querySelector("#contact-form").addEventListener("submit", async function (event) {
     event.preventDefault(); // Prevent the default form submission
 
@@ -93,9 +73,4 @@ document.querySelectorAll("#contact-form input, #contact-form textarea").forEach
         const form = document.querySelector("#contact-form");
         form.style.display = "block"; // Show the form again
     });
-});
-
-
-        messageDiv.textContent = "Oops! Something went wrong. Please try again.";
-    }
 });
